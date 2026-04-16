e58a2a6d59f5f66378e9c58b22c2c19653320f60
JeroCordoba <jerof.l.cordoba01@gmail.com>
2026-04-15 23:49:01 -0300

    Clase 4 hasta recreo

---
+++ clase-04-formularios/src/app/pages/registro/registro.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { Validators, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  imports: [ReactiveFormsModule],
+  templateUrl: './registro.html',
+  styleUrl: './registro.css',
+})
+export class Registro {
+  /*nombre = new FormControl('Valor por defecto',{});
+  apellido = new FormControl();
+  email = new FormControl();
+  password = new FormControl();
+
+  mostrar(){
+    console.log(this.nombre.value, this.apellido.value, this.email.value, this.password.value);
+  }*/
+
+  formulario = new FormGroup({
+    nombre: new FormControl("Valor por defecto", {
+      validators:[Validators.minLength(3), Validators.maxLength(15), Validators.required],
+    }),
+    apellido: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.email, Validators.required]),
+    password: new FormControl('',[Validators.required, Validators.minLength(8)]),
+  });
+  mostrar(){
+    if (!this.formulario.valid) {
+      console.log("No valido");
+      console.log(this.formulario.errors);
+      console.log(this.formulario.getError("maxlength"));
+      console.log(this.formulario.hasError("maxlength"));
+      console.log(this.formulario.get("nombre"));
+    } else {
+    console.log(this.formulario.value);
+    }
+  }
+}
